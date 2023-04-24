22d3ba570eee58cf695144d16e416ec114286ff9
Uzair Saiyed <uzairhajra76330@gmail.com>
2023-04-24 22:14:53 +0530

    Initial Commit

---
+++ frontend/pages/_app.js
@@ -0,0 +1,158 @@
+import { useEffect } from "react";
+import Head from "next/head";
+import Navbar from "../components/Navbar/Navbar";
+import dynamic from "next/dynamic";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { NotificationProvider } from "web3uikit";
+import { AnimatePresence, motion } from "framer-motion";
+import { useRouter } from "next/router";
+import { fantom, fantomTestnet } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+import { MoralisProvider } from "react-moralis";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+import "../styles/globals.css";
+import Script from "next/script";
+const thetaTestnetChain = {
+  id: 365,
+  name: "Theta Testnet",
+  network: "Theta Testnet",
+
+  rpcUrls: {
+    default: {
+      http: [" https://eth-rpc-api-testnet.thetatoken.org/rpc"],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: "theta-testnet",
+      url: "https://testnet-explorer.thetatoken.org",
+    },
+  },
+
+  testnet: true,
+};
+const thetaChain = {
+  id: 361,
+  name: "Theta",
+  network: "Theta",
+
+  rpcUrls: {
+    default: {
+      http: [" https://eth-rpc-api.thetatoken.org/rpc"],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: "theta-testnet",
+      url: "https://explorer.thetatoken.org",
+    },
+  },
+
+  testnet: false,
+};
+
+const { chains, provider } = configureChains(
+  [thetaTestnetChain, thetaChain],
+  [
+    jsonRpcProvider({
+      rpc: chain => ({ http: chain.rpcUrls.default.http[0] }),
+    }),
+  ]
+);
+const { connectors } = getDefaultWallets({
+  appName: "Patreon-Web3",
+  chains,
+});
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {}, []);
+  const router = useRouter();
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+        <meta property="og:title" content="Home" key="title" />
+        <link
+          rel="stylesheet"
+          href="https://assets.codepen.io/7773162/swiper-bundle.min.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          crossorigin="anonymous"
+        />
+        <link
+          rel="stylesheet"
+          href="https://use.typekit.net/hmg7hbs.css"
+          type="text/css"
+        />
+        <link
+          href="https://c5.patreon.com/external/fonts/gt-walsheim.css"
+          rel="stylesheet"
+          type="text/css"
+        />
+
+        <script
+          src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+          integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+          crossorigin="anonymous"
+        ></script>
+        <script
+          src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+          integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+          crossorigin="anonymous"
+        ></script>
+        <script
+          src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+          integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+          crossorigin="anonymous"
+        ></script>
+      </Head>
+      <div className="cursor"></div>
+
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chains}>
+          <MoralisProvider initializeOnMount={false}>
+            <Navbar></Navbar>
+            <NotificationProvider>
+              <AnimatePresence mode="wait" />
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{
+                  opacity: 1,
+                  transition: {
+                    delay: 0.25,
+                    duration: 0.5,
+                  },
+                }}
+                exit={{
+                  opacity: 0,
+                  backgroundColor: "transparent",
+
+                  transition: {
+                    delay: 0.25,
+                    ease: "easeInOut",
+                  },
+                }}
+                key={router.route}
+                className="content"
+              >
+                <Component {...pageProps} />
+              </motion.div>
+            </NotificationProvider>
+          </MoralisProvider>
+        </RainbowKitProvider>
+      </WagmiConfig>
+    </>
+  );
+}
+
+export default MyApp;
---
+++ frontend/pages/index.js
@@ -0,0 +1,339 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Footer from "../components/Footer/Footer";
+import React, { Component } from "react";
+import PatreonHello from "../assets/img/patreon-hero-illustration.png";
+import PatreonPredictable from "../assets/img/patreon-predictablerevenue.png";
+import PatreonConnection from "../assets/img/patreon-fanconnection.png";
+import PatreonBenefits from "../assets/img/patreon-exclusivebenefits.png";
+import PatreonRae from "../assets/img/issa-rae.png";
+import Step1 from "../assets/img/step1.svg";
+import Step2 from "../assets/img/step2.svg";
+import Step3 from "../assets/img/step3-top.svg";
+import Step3Bottom from "../assets/img/step3-bottom.svg";
+
+const getStyle = {
+  fontHello: {
+    fontWeight: "900",
+    fontSize: "3rem",
+  },
+  fontParagraph: {
+    fontSize: "1.4rem",
+    color: "grey",
+  },
+  helloButton: {
+    borderRadius: "20px",
+    padding: "10px 20px",
+  },
+  fontCreator: {
+    fontWeight: "700",
+    fontSize: "2.5rem",
+  },
+  cardSmoke: {
+    backgroundColor: "whitesmoke",
+    border: "none",
+  },
+  backgroundVideoImage: {
+    backgroundImage:
+      "url(https://c5.patreon.com/external/marketing/vertical_images/video-v4-issarae.jpg)",
+    width: "100%",
+    backgroundPosition: "center center",
+    backgroundSize: "cover",
+  },
+  videoImageLink: {
+    display: "block",
+    padding: "5px",
+    fontSize: "1.5rem",
+    fontWeight: "700",
+    color: "black",
+  },
+};
+
+export default function Home() {
+  return (
+    <>
+      <div className="container pt-5">
+        <div className="row pt-5">
+          <div className="col-md-6">
+            <br />
+            <SectionHello getStyle={getStyle} />
+          </div>
+          <div className="col-md-6">
+            <Image
+              src={PatreonHello}
+              alt="imagehello"
+              style={{ maxWidth: "100%" }}
+            />
+          </div>
+        </div>
+        <div className="text-center mt-5">
+          <h2 style={getStyle.fontCreator}>In the words or our creators</h2>
+        </div>
+        <CreatorCards style={getStyle} />
+      </div>
+      <MembershipBenefits />
+      <WhoUse getStyle={getStyle} />
+      <HowDoes />
+      <Footer />
+    </>
+  );
+}
+function SectionHello(props) {
+  return (
+    <React.Fragment>
+      <h2 style={props.getStyle.fontHello}>CREATE ON YOUR OWN TERMS</h2>
+      <p style={props.getStyle.fontParagraph} className="mt-5">
+        Start a membership business to develop a direct relationship with your
+        biggest fans and generate predictable, recurring revenue from your
+        creative work.
+      </p>
+      <button
+        className="btn btn-danger mt-2"
+        style={props.getStyle.helloButton}
+      >
+        Get Started
+      </button>
+    </React.Fragment>
+  );
+}
+
+function CreatorCards(props) {
+  return (
+    <React.Fragment>
+      <div className="card-deck mt-5 mb-5">
+        <Card style={props.style} />
+        <Card style={props.style} />
+        <Card style={props.style} />
+      </div>
+    </React.Fragment>
+  );
+}
+
+function Card(props) {
+  return (
+    <div className="card">
+      <div className="card-body" style={props.style.cardSmoke}>
+        <p className="card-text p-3">
+          "Membership is a relationship between you and your most engaged fans —
+          the ones that choose to go a level deeper than just following you on
+          social media. They become paying patrons in exchange for exclusive
+          benefits you offer."
+        </p>
+      </div>
+      <div className="card-footer" style={props.style.cardSmoke}>
+        <div className="card-text p-3">
+          <Image src={PatreonRae} alt="Rae" style={{ width: "15%" }} /> &nbsp;
+          <strong>Issa Belle</strong>, Video Creator
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function MembershipBenefits() {
+  return (
+    <div style={{ backgroundColor: "whitesmoke" }}>
+      <div className="container mt-3 p-5">
+        <div className="text-center mt-5 mb-5">
+          <h1>Why Membership?</h1>
+          <p>
+            Patreon is a creator-founded company, helping creators build
+            membership <br />
+            businesses that empower them to create on their own terms.
+          </p>
+        </div>
+        <div className="row mt-5">
+          <div className="col-md-4 p-4 text-center">
+            <Image src={PatreonPredictable} alt="" />
+            <div className="text-center">
+              <h4>
+                <b>Predictable Revenue</b>
+              </h4>
+              <p>
+                Generate predictable, recurring revenue from your work through
+                fans who pay you monthly
+              </p>
+            </div>
+          </div>
+          <div className="col-md-4 p-4 text-center">
+            <Image src={PatreonConnection} alt="" />
+            <div className="text-center">
+              <h4>
+                <b>Fan Connection</b>
+              </h4>
+              <p>
+                Connect with your biggest fans directly, free from the ads and
+                algorithms that drive social media
+              </p>
+            </div>
+          </div>
+          <div className="col-md-4 p-4 text-center">
+            <Image src={PatreonBenefits} alt="" />
+            <div className="text-center">
+              <h4>
+                <b>Exclusive Benefits</b>
+              </h4>
+              <p>
+                Give your fans what they want: a peek behind the scenes, extras
+                they can’t get elsewhere, and the pride of fueling what you do
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function WhoUse(props) {
+  return (
+    <div className="container mt-5 mb-5 pb-5">
+      <div className="text-center pt-5 pb-5">
+        <h1>
+          <b>Who uses Patreon</b>
+        </h1>
+      </div>
+      <div className="row">
+        <div
+          className="col-md-6"
+          style={props.getStyle.backgroundVideoImage}
+        ></div>
+        <div className="col-md-6">
+          <a href="/" style={props.getStyle.videoImageLink}>
+            Podcaster
+          </a>
+          <a href="/" style={props.getStyle.videoImageLink}>
+            Video Creators
+          </a>
+          <a href="/" style={props.getStyle.videoImageLink}>
+            Musicians
+          </a>
+          <a href="/" style={props.getStyle.videoImageLink}>
+            Visual Artists
+          </a>
+          <a href="/" style={props.getStyle.videoImageLink}>
+            Communities
+          </a>
+          <a href="/" style={props.getStyle.videoImageLink}>
+            Writers & Journalists
+          </a>
+          <a href="/" style={props.getStyle.videoImageLink}>
+            Creators-of-all-kinds
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function HowDoes(props) {
+  return (
+    <div style={{ backgroundColor: "whitesmoke" }} className="p-5">
+      <div className="container">
+        <div className="text-center pt-5">
+          <h1>How does it work?</h1>
+          <div className="row">
+            <div className="col-md-2"></div>
+            <div className="col-md-8 mt-4">
+              <p>
+                Membership is a relationship between you and your most engaged
+                fans — the ones that choose to go a level deeper than just
+                following you on social media. They become paying patrons in
+                exchange for exclusive benefits you offer.
+              </p>
+            </div>
+          </div>
+        </div>
+        <Step />
+      </div>
+    </div>
+  );
+}
+
+function Step(props) {
+  const stepStyle = {
+    stepText: {
+      padding: "40px",
+      marginTop: "10px",
+    },
+  };
+
+  function LeftStep(props) {
+    return (
+      <div className="row mt-4 mb-4">
+        <div className="col-md-6 text-center">
+          <Image
+            src={props.image}
+            alt="step_1_image"
+            style={{ maxWidth: "100%" }}
+          />
+          {props.image2 ? (
+            <Image src={props.image2} alt="second_image" />
+          ) : null}
+        </div>
+        <div className="col-md-6">
+          <div style={stepStyle.stepText}>{props.children}</div>
+        </div>
+      </div>
+    );
+  }
+
+  function RightStep(props) {
+    return (
+      <div className="row mt-4 mb-4">
+        <div className="col-md-6">
+          <div style={stepStyle.stepText}>{props.children}</div>
+        </div>
+        <div className="col-md-6 text-center">
+          <Image
+            src={props.image}
+            alt="step_1_image"
+            style={{ maxWidth: "100%" }}
+          />
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <React.Fragment>
+      <LeftStep image={Step1}>
+        <React.Fragment>
+          <p>STEP 1</p>
+          <h2>
+            <b>Tell your fans you're launching on Patreon.</b>
+          </h2>
+          <p>
+            Reach out to fans on your social channels to find out more about
+            what kind of membership benefits they’d be interested in.
+          </p>
+        </React.Fragment>
+      </LeftStep>
+      <RightStep image={Step2}>
+        <p>STEP 2</p>
+        <h2>
+          <b>Set up your page and membership tiers, and launch!</b>
+        </h2>
+        <p>
+          Each tier has different benefits for different levels of engagement
+          with you. When you’re ready, launch your membership and tell your
+          fans.
+        </p>
+      </RightStep>
+      <LeftStep image={Step3} image2={Step3Bottom}>
+        <React.Fragment>
+          <p>STEP 3</p>
+          <h2>
+            <b>Develop relationships by taking them along on the journey.</b>
+          </h2>
+          <p>
+            Share updates, deliver benefits, and build community. Keep up the
+            momentum by sharing on your social channels to bring more fans to
+            Patreon.
+          </p>
+        </React.Fragment>
+      </LeftStep>
+    </React.Fragment>
+  );
+}
