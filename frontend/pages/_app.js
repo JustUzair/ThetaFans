bcd6ad219b1e6668b1e6f4181b86b278e807611c
Uzair Saiyed <uzairhajra76330@gmail.com>
2023-04-25 20:27:13 +0530

    Added Animated Background

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -80,8 +80,8 @@ function MyApp({ Component, pageProps }) {
         src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
         crossOrigin="anonymous"
-        defer
       ></Script>
+
       <Script
         src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
         integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
@@ -89,11 +89,11 @@ function MyApp({ Component, pageProps }) {
         defer
       ></Script>
       <Script
-        src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/js/bootstrap.min.js"
+        integrity="sha512-1/RvZTcCDEUjY/CypiMz+iqqtaoQfAITmNSJY17Myp4Ms5mdxPS5UV7iOfdZoxcGhzFbOm6sntTKJppjvuhg4g=="
         crossOrigin="anonymous"
-        defer
       ></Script>
+
       <Head>
         <title>Home</title>
         <meta property="og:title" content="Home" key="title" />
@@ -147,6 +147,30 @@ function MyApp({ Component, pageProps }) {
                 key={router.route}
                 className="content"
               >
+                <div className="wrapper">
+                  <div className="box">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                  </div>
+                </div>
                 <Component {...pageProps} />
               </motion.div>
             </NotificationProvider>
--- frontend/pages/explore.js
+++ frontend/pages/explore.js
@@ -1,7 +1,210 @@
 import React from "react";
 
 const explore = () => {
-  return <div>explore</div>;
+  return (
+    <div className="shell">
+      <div className="card-container">
+        <div className="creator-card">
+          <div className="wsk-cp-img">
+            <img
+              src="https://3.bp.blogspot.com/-eDeTttUjHxI/WVSvmI-552I/AAAAAAAAAKw/0T3LN6jABKMyEkTRUUQMFxpe6PLvtcMMwCPcBGAYYCw/s1600/001-culture-clash-matthew-gianoulis.jpg"
+              alt="Product"
+              className="img-responsive"
+            />
+          </div>
+          <div className="wsk-cp-text">
+            <div className="category">
+              <span>Ethnic</span>
+            </div>
+            <div className="title-product">
+              <h3>My face not my heart</h3>
+            </div>
+            <div className="description-prod">
+              <p>
+                Description Product tell me how to change playlist height size
+                like 600px in html5 player. player good work now check this link
+              </p>
+            </div>
+            <div className="card-footer">
+              <div className="wcf-left">
+                <span className="price">Rp500.000</span>
+              </div>
+              <div className="wcf-right">
+                <a href="#" className="buy-btn">
+                  <i className="zmdi zmdi-shopping-basket"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="creator-card">
+          <div className="wsk-cp-img">
+            <img
+              src="https://3.bp.blogspot.com/-eDeTttUjHxI/WVSvmI-552I/AAAAAAAAAKw/0T3LN6jABKMyEkTRUUQMFxpe6PLvtcMMwCPcBGAYYCw/s1600/001-culture-clash-matthew-gianoulis.jpg"
+              alt="Product"
+              className="img-responsive"
+            />
+          </div>
+          <div className="wsk-cp-text">
+            <div className="category">
+              <span>Ethnic</span>
+            </div>
+            <div className="title-product">
+              <h3>My face not my heart</h3>
+            </div>
+            <div className="description-prod">
+              <p>
+                Description Product tell me how to change playlist height size
+                like 600px in html5 player. player good work now check this link
+              </p>
+            </div>
+            <div className="card-footer">
+              <div className="wcf-left">
+                <span className="price">Rp500.000</span>
+              </div>
+              <div className="wcf-right">
+                <a href="#" className="buy-btn">
+                  <i className="zmdi zmdi-shopping-basket"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="creator-card">
+          <div className="wsk-cp-img">
+            <img
+              src="https://3.bp.blogspot.com/-eDeTttUjHxI/WVSvmI-552I/AAAAAAAAAKw/0T3LN6jABKMyEkTRUUQMFxpe6PLvtcMMwCPcBGAYYCw/s1600/001-culture-clash-matthew-gianoulis.jpg"
+              alt="Product"
+              className="img-responsive"
+            />
+          </div>
+          <div className="wsk-cp-text">
+            <div className="category">
+              <span>Ethnic</span>
+            </div>
+            <div className="title-product">
+              <h3>My face not my heart</h3>
+            </div>
+            <div className="description-prod">
+              <p>
+                Description Product tell me how to change playlist height size
+                like 600px in html5 player. player good work now check this link
+              </p>
+            </div>
+            <div className="card-footer">
+              <div className="wcf-left">
+                <span className="price">Rp500.000</span>
+              </div>
+              <div className="wcf-right">
+                <a href="#" className="buy-btn">
+                  <i className="zmdi zmdi-shopping-basket"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="creator-card">
+          <div className="wsk-cp-img">
+            <img
+              src="https://3.bp.blogspot.com/-eDeTttUjHxI/WVSvmI-552I/AAAAAAAAAKw/0T3LN6jABKMyEkTRUUQMFxpe6PLvtcMMwCPcBGAYYCw/s1600/001-culture-clash-matthew-gianoulis.jpg"
+              alt="Product"
+              className="img-responsive"
+            />
+          </div>
+          <div className="wsk-cp-text">
+            <div className="category">
+              <span>Ethnic</span>
+            </div>
+            <div className="title-product">
+              <h3>My face not my heart</h3>
+            </div>
+            <div className="description-prod">
+              <p>
+                Description Product tell me how to change playlist height size
+                like 600px in html5 player. player good work now check this link
+              </p>
+            </div>
+            <div className="card-footer">
+              <div className="wcf-left">
+                <span className="price">Rp500.000</span>
+              </div>
+              <div className="wcf-right">
+                <a href="#" className="buy-btn">
+                  <i className="zmdi zmdi-shopping-basket"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="creator-card">
+          <div className="wsk-cp-img">
+            <img
+              src="https://3.bp.blogspot.com/-eDeTttUjHxI/WVSvmI-552I/AAAAAAAAAKw/0T3LN6jABKMyEkTRUUQMFxpe6PLvtcMMwCPcBGAYYCw/s1600/001-culture-clash-matthew-gianoulis.jpg"
+              alt="Product"
+              className="img-responsive"
+            />
+          </div>
+          <div className="wsk-cp-text">
+            <div className="category">
+              <span>Ethnic</span>
+            </div>
+            <div className="title-product">
+              <h3>My face not my heart</h3>
+            </div>
+            <div className="description-prod">
+              <p>
+                Description Product tell me how to change playlist height size
+                like 600px in html5 player. player good work now check this link
+              </p>
+            </div>
+            <div className="card-footer">
+              <div className="wcf-left">
+                <span className="price">Rp500.000</span>
+              </div>
+              <div className="wcf-right">
+                <a href="#" className="buy-btn">
+                  <i className="zmdi zmdi-shopping-basket"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="creator-card">
+          <div className="wsk-cp-img">
+            <img
+              src="https://3.bp.blogspot.com/-eDeTttUjHxI/WVSvmI-552I/AAAAAAAAAKw/0T3LN6jABKMyEkTRUUQMFxpe6PLvtcMMwCPcBGAYYCw/s1600/001-culture-clash-matthew-gianoulis.jpg"
+              alt="Product"
+              className="img-responsive"
+            />
+          </div>
+          <div className="wsk-cp-text">
+            <div className="category">
+              <span>Ethnic</span>
+            </div>
+            <div className="title-product">
+              <h3>My face not my heart</h3>
+            </div>
+            <div className="description-prod">
+              <p>
+                Description Product tell me how to change playlist height size
+                like 600px in html5 player. player good work now check this link
+              </p>
+            </div>
+            <div className="card-footer">
+              <div className="wcf-left">
+                <span className="price">Rp500.000</span>
+              </div>
+              <div className="wcf-right">
+                <a href="#" className="buy-btn">
+                  <i className="zmdi zmdi-shopping-basket"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default explore;
