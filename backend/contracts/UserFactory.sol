f497d6a080f6c00e9b0a653f864f86cf956c42df
Jazthedev <joelpepa2@gmail.com>
2023-05-08 14:17:07 +0200

    fix factory contract for tiers

--- backend/contracts/UserFactory.sol
+++ backend/contracts/UserFactory.sol
@@ -17,24 +17,22 @@ contract UserFactory {
     }
 
     function createProfile(string memory _tokenName,string memory _tokenSymbol,
-    string memory _name,string memory _description,uint _subscriptionAmount) public
+    string memory _name,string memory _description,uint _bronzePrice,uint _silverPrice,uint _goldPrice) public
     {
         address profileAddress = userProfile[_name];
         require(creatorContract[msg.sender] == address(0),"address already has an account");
         require(profileAddress == address(0));
-        UserProfile newUser = new UserProfile(_tokenName,_tokenSymbol,msg.sender,_name,_description,_subscriptionAmount);
+        UserProfile newUser = new UserProfile(_tokenName,_tokenSymbol,msg.sender,_name,_description, _bronzePrice, _silverPrice, _goldPrice);
         address addr = address(newUser);
         //add the created profile to store data variables
         createdProfiles.push(addr);
         userProfile[_name] = addr;
         creatorContract[msg.sender] = addr;
     }
-    function isSignedUp(address _creatorAddress) external view returns(bool){
-        if (creatorContract[_creatorAddress] == address(0)) return false;
-        return true;
-    }
-    function getCreatorContractAddress(address _creatorAddress) external view returns(address){
-        return creatorContract[_creatorAddress];
+    function isSignedUp(address _creatorAddress,string memory _name) external view returns(bool){
+        address profileAddress = userProfile[_name];
+        if(profileAddress == _creatorAddress) return true;
+        return false;
     }
     function working() public pure returns(string memory){
         return "yes smartcontract deployed";
@@ -43,10 +41,10 @@ contract UserFactory {
         return createdProfiles;
     }
 
-    function getCreator(address _creator) public view returns(string memory,string memory,uint){
+    function getCreator(address _creator) public view returns(string memory,string memory){
         return UserProfile(_creator).getCreatorInfo();
     }
-    function getCreatorOld(address _creator) public view returns(string memory,string memory,uint,uint,uint,uint){
+    function getCreatorOld(address _creator) public view returns(string memory,string memory,uint,uint,uint,UserProfile.Tier memory,UserProfile.Tier memory,UserProfile.Tier memory){
         return UserProfile(_creator).getProfileData();
     }
 }
